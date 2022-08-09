4e31760b05fd428c1b7ec3dd7e1f3c1746cd8568
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-08-09 23:00:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Killy's Movie Watchlist
+
+> This is a webapp that displays a list of movies to watch. It allwos users to search movies and then save them for later, this is saved in local storage so the user can access it anytime. All search calls are made to the OMDB API.
+
+![Screenshot](screenshot.png)
+
+## Live Demo
+
+[Live Demo Link](https://killy10o10.github.io/movie-watchlist)
+
+## Built With
+
+- HTML
+- Sass
+- javascript
+- Webpack
+
+## Author
+
+👤 **Okine Kingsley**
+
+- GitHub: [@killy10o10](https://github.com/killy10o10)
+- Twitter: [@Quami_Killy](https://twitter.com/Quami_Killy)
+- Instagram: [quami_killy](https://www.instagram.com/quami_killy/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone who's code/resource was used
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Muvi WebApp',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+};
